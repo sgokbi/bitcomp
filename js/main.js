50f83733240cc8f84ccf0c1a618f5fb365338db6
unknown <sagorika.tech@gmail.com>
2022-09-03 15:04:17 +0600

    home page done

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 
 // ==== NAVBAR ====
 const navBar = document.querySelector(".navbar-nav");
-console.log(navBar);
+
 // const toggleButton = document.querySelector(".navbar-toggler");
 const hideBar = document.querySelector(".hide-bar");
 const showBar = document.querySelector(".show-bar");
@@ -17,4 +17,20 @@ showBar.addEventListener("click", function () {
 hideBar.addEventListener("click", function () {
   hideBar.style.display = "none";
   showBar.style.display = "block";
+});
+
+// ////////// CONTACT SECTION
+
+// const plusCircle = document.querySelector(".plus-circle");
+// const minusCircle = document.querySelector(".minus-circle");
+// console.log(plusCircle);
+// const hiddenAnswerBox = document.querySelector(".hidden-answer-box");
+
+// plusCircle.addEventListener("click", function () {
+//   console.log("click");
+//   hiddenAnswerBox.style.display = "block";
+// });
+
+document.querySelector(".FINCSC-img").addEventListener("click", function () {
+  console.log("click");
 });
