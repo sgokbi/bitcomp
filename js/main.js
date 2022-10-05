1f19d1cd7fa8387d577d86f2f614741fd6665c6b
unknown <sagorika.tech@gmail.com>
2022-10-05 15:18:13 +0600

    implement some js in foresta page

--- js/main.js
+++ js/main.js
@@ -20,15 +20,44 @@ hideBar.addEventListener("click", function () {
 });
 
 /* ====================================================================
+       FORESTA PAGE start 
+====================================================================   */
+const bbb = document.querySelector(".all-systems-tools");
+console.log(bbb);
+const buttons = document.querySelectorAll(".button");
+console.log(buttons);
+const contents = document.querySelectorAll(".content");
+console.log(contents);
+
+for (let i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", function () {
+    console.log(buttons[i]);
+    console.log(contents[i]);
+
+    for (const content of contents) {
+      content.classList.remove("active");
+      contents[i].classList.add("active");
+    }
+
+    for (const btn of buttons) {
+      btn.classList.remove("active");
+      buttons[i].classList.add("active");
+    }
+  });
+}
+
+/* ====================================================================
               INFORMATION SYSTEM PAGE start 
 ====================================================================   */
 // <!-- ======= 	BITCAMP GEO DATA SECTION  ========  -->
-document
-  .querySelector(".geo-data-logo-img")
-  .addEventListener("click", function () {
-    window.location.href =
-      "https://bitcomp.com/wp-content/uploads/2018/11/geodata.jpg";
-  });
+const geoData = document.querySelector(".geo-data-logo-img");
+console.log(geoData);
+console.log("cccc");
+
+geoData.addEventListener("click", function () {
+  window.location.href =
+    "https://bitcomp.com/wp-content/uploads/2018/11/geodata.jpg";
+});
 
 const leafPoint = document.querySelector("#leaf-point-logo");
 leafPoint.addEventListener("click", () => {
